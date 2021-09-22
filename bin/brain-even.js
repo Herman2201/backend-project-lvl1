11c01b41451f77304a067c508a4656be7b961e8c
Herman <ushkalo.herman@gmail.com>
2021-09-22 03:22:05 +0300

    complete brain-even, fix name export

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
 import readlineSync from 'readline-sync';
+import name from '../src/cli.js';
 
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+console.log(`Answer 'yes' if the number is even, otherwise answer 'no'.`);
+const evenNumber = (a) => a % 2 === 0 ? 1 : 0;
+const mathRandom = () => Math.floor((Math.random() * 100) + 1);
+const ded = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const a = mathRandom();
+    console.log(`Question: ${a}`);
+    const ansver = readlineSync.question('Your answer: ');
+    if (evenNumber(a) === 1) {
+      if (ansver === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`${ansver} is wrong answer ;(. Correct answer was 'yes'.`);
+        return console.log(`Let's try again, ${name}`);
+      }
+    }
+    if (evenNumber(a) === 0) {
+      if (ansver === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`${ansver} is wrong answer ;(. Correct answer was 'no'.`);
+        return console.log(`Let's try again, ${name}`);
+      }
+    }
+  }
+  console.log(`Congratulations, ${name}`);
+  return 0;
+};
+ded();
